30c14f8029923df9bc89966d60f5185c046a7b97
zsevvaldaysali <sevval.daysali@outlook.com>
2023-08-23 11:40:54 +0300

    detach tab bar and tab bar view

--- lib/features/todo/pages/home_page.dart
+++ lib/features/todo/pages/home_page.dart
@@ -92,6 +92,113 @@ class _HomePageState extends ConsumerState<HomePage> with TickerProviderStateMix
           ),
         ),
       ),
-      );
+      body: SafeArea(
+        child: Padding(
+          padding: EdgeInsets.symmetric(horizontal: 20.w),
+          child: ListView(
+            children: [
+              HeightSpacer(height: 5.h),
+              Row(
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  Icon(
+                    FontAwesome.tasks,
+                    size: 20.w,
+                    color: AppConstants.kLight,
+                  ),
+                  WidthSpacer(width: 10.w),
+                  ReusableTextWidget(
+                    text: "Today's Task",
+                    style: appStyleWidget(
+                      18,
+                      AppConstants.kLight,
+                      FontWeight.bold,
+                    ),
+                  ),
+                ],
+              ),
+              HeightSpacer(height: 25.h),
+              Container(
+                decoration: BoxDecoration(
+                  color: AppConstants.kLight,
+                  borderRadius: BorderRadius.all(
+                    Radius.circular(AppConstants.kRadius),
+                  ),
+                ),
+                child: TabBar(
+                    isScrollable: false,
+                    labelColor: AppConstants.kBlueLight,
+                    labelStyle: appStyleWidget(24, AppConstants.kBlueLight, FontWeight.w700),
+                    labelPadding: EdgeInsets.zero,
+                    unselectedLabelColor: AppConstants.kLight,
+                    indicator: BoxDecoration(
+                      color: const Color.fromARGB(255, 18, 174, 72),
+                      borderRadius: BorderRadius.all(
+                        Radius.circular(AppConstants.kRadius),
+                      ),
+                    ),
+                    indicatorSize: TabBarIndicatorSize.label,
+                    controller: tabController,
+                    tabs: [
+                      Tab(
+                        child: SizedBox(
+                          width: AppConstants.kWidth * 0.5,
+                          child: Center(
+                            child: ReusableTextWidget(
+                              text: "Pending",
+                              style: appStyleWidget(
+                                16,
+                                AppConstants.kDarkBk,
+                                FontWeight.bold,
+                              ),
+                            ),
+                          ),
+                        ),
+                      ),
+                      Tab(
+                        child: SizedBox(
+                          width: AppConstants.kWidth * 0.5,
+                          child: Center(
+                            child: ReusableTextWidget(
+                              text: "Completed",
+                              style: appStyleWidget(
+                                16,
+                                AppConstants.kDarkBk,
+                                FontWeight.bold,
+                              ),
+                            ),
+                          ),
+                        ),
+                      ),
+                    ]),
+              ),
+              HeightSpacer(height: 20.h),
+              SizedBox(
+                height: AppConstants.kHeight * 0.3,
+                width: AppConstants.kWidth,
+                child: ClipRRect(
+                  borderRadius: BorderRadius.all(
+                    Radius.circular(AppConstants.kRadius),
+                  ),
+                  child: TabBarView(
+                    controller: tabController,
+                    children: [
+                      Container(
+                        color: AppConstants.kLightBk,
+                        height: AppConstants.kHeight * 0.3,
+                      ),
+                      Container(
+                        color: AppConstants.kLightBk,
+                        height: AppConstants.kHeight * 0.3,
+                      ),
+                    ],
+                  ),
+                ),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
