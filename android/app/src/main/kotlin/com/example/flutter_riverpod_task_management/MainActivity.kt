10b5a3cca21eb08b81435dbb1c639892fce492c6
zsevvaldaysali <sevval.daysali@outlook.com>
2023-08-23 11:17:21 +0300

    initialize flutter screen utils

---
+++ android/app/src/main/kotlin/com/example/flutter_riverpod_task_management/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.flutter_riverpod_task_management
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod_task_management/common/utils/constants.dart';
+import 'package:flutter_riverpod_task_management/features/todo/pages/home_page.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+
+import 'features/onboarding/pages/onboarding_page.dart';
+
+void main() {
+  runApp(const ProviderScope(child: MyApp()));
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return ScreenUtilInit(
+        useInheritedMediaQuery: true,
+        designSize: const Size(375, 825),
+        minTextAdapt: true,
+        builder: (context, child) {
+          return MaterialApp(
+            debugShowCheckedModeBanner: false,
+            title: 'Todo App',
+            theme: ThemeData(
+                colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+                useMaterial3: true,
+                scaffoldBackgroundColor: AppConstants.kDarkBk),
+            themeMode: ThemeMode.dark,
+            home: HomePage(),
+          );
+        });
+  }
+}
