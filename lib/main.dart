0aac10043f4d90b46a3307aaab4a044089ab1d42
zsevvaldaysali <sevval.daysali@outlook.com>
2023-08-29 15:51:37 +0300

    firebase setup with CLI and getting the SHA1 and SHA256 keys

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,21 @@
 import 'package:dynamic_color/dynamic_color.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod_task_management/common/utils/constants.dart';
 import 'package:flutter_riverpod_task_management/features/todo/pages/home_page.dart';
+import 'package:flutter_riverpod_task_management/firebase_options.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 
-void main() {
+
+//com.googleusercontent.apps.440506479780-kh9up6omd97td97qsc0u1u3o7t5b377s
+
+
+void main() async{
+  WidgetsFlutterBinding.ensureInitialized();
+ await Firebase.initializeApp(
+  options: DefaultFirebaseOptions.currentPlatform,
+ );
   runApp(const ProviderScope(child: MyApp()));
 }
 
--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -6,12 +6,14 @@ import FlutterMacOS
 import Foundation
 
 import dynamic_color
+import firebase_core
 import path_provider_foundation
 import smart_auth
 import sqflite
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   DynamicColorPlugin.register(with: registry.registrar(forPlugin: "DynamicColorPlugin"))
+  FLTFirebaseCorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseCorePlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
   SmartAuthPlugin.register(with: registry.registrar(forPlugin: "SmartAuthPlugin"))
   SqflitePlugin.register(with: registry.registrar(forPlugin: "SqflitePlugin"))
